a83f787ccb861ed90f0a57b6c085d820dfc0b9ec
observerss <jingchaohu@gmail.com>
2012-01-05 08:35:29 +0800

    first commit

---
+++ filter.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from collections import defaultdict
+import re
+import os
+
+__all__ = ['NaiveFilter','BSFilter','DFAFilter']
+__author__ = 'observer'
+__date__ = '2012.01.05'
+
+class NaiveFilter():
+    '''Filter Messages from keywords
+
+    very simple filter implementation
+    
+    >>> f = Filter()
+    >>> f = Filter()
+    >>> f.add("sexy")
+    >>> f.filter("hello sexy baby")
+    hello **** baby
+    '''
+    def __init__(self):
+        self.keywords = set([])
+    
+    def parse(self,path):
+        for keyword in open(path):
+            self.keywords.add(keyword.strip().decode('utf-8'))
+
+    def filter(self,message,repl="*"):
+        message = unicode(message)
+        for kw in self.keywords:
+            message = message.replace(kw,repl)
+        return message
+                
+
+class BSFilter:
+    '''Filter Messages from keywords
+
+    Use Back Sorted Mapping to reduce replacement times
+    
+    >>> f = Filter()
+    >>> f.add("sexy")
+    >>> f.filter("hello sexy baby")
+    hello **** baby
+    '''
+    def __init__(self):
+        self.keywords = []
+        self.kwsets = set([])
+        self.bsdict = defaultdict(set)
+        self.pat_en = re.compile(r'^[0-9a-zA-Z]+$') # english phrase or not
+   
+    def add(self,keyword):
+        if not isinstance(keyword,unicode):
+            keyword = keyword.decode('utf-8')
+        if keyword not in self.kwsets:
+            self.keywords.append(keyword)
+            self.kwsets.add(keyword)
+            index = len(self.keywords) - 1
+            for word in keyword.split():
+                if self.pat_en.search(word):
+                    self.bsdict[word].add(index)
+                else:
+                    for char in word:
+                        self.bsdict[char].add(index)
+
+    def parse(self,path):
+        with open(path,"r") as f:
+            for keyword in f:
+                self.add(keyword.strip())
+
+    def filter(self,message,repl="*"):
+        if not isinstance(message,unicode):
+            message = message.decode('utf-8')
+        for word in message.split():
+            if self.pat_en.search(word):
+                for index in self.bsdict[word]:
+                    message = message.replace( self.keywords[index], repl )
+            else:
+                for char in word:
+                    for index in self.bsdict[char]:
+                        message = message.replace( self.keywords[index], repl )
+        return message
+
+class DFAFilter():
+    '''Filter Messages from keywords
+
+    Use DFA to keep algorithm perform constantly
+    
+    >>> f = Filter()
+    >>> f.add("sexy")
+    >>> f.filter("hello sexy baby")
+    hello **** baby
+    '''
+    def __init__(self):
+        self.keyword_chains = {}
+        self.delimit = '\x00'
+    
+    def add(self,keyword):
+        if not isinstance(keyword,unicode):
+            keyword = keyword.decode('utf-8')
+        chars = keyword.strip()
+        if not chars:
+            return
+        level = self.keyword_chains
+        for i in range(len(chars)):
+            if chars[i] in level:
+                level = level[ chars[i] ]
+            else:
+                if not isinstance(level,dict):
+                    break
+                for j in range(i,len(chars)):
+                    level[ chars[j] ] = {}
+                    last_level,last_char = level,chars[j]
+                    level = level[ chars[j] ]
+                last_level[last_char] = {self.delimit:0}
+                break
+        if i == len(chars)-1:
+            level[self.delimit] = 0
+
+    
+    def parse(self,path):
+        with open(path) as f:
+            for keyword in f:
+                self.add(keyword.strip())
+
+    def filter(self,message,repl="*"):
+        if not isinstance(message,unicode):
+            message = message.decode('utf-8')
+
+        ret = []
+        start = 0
+        while start < len(message):
+            level = self.keyword_chains
+            step_ins = 0
+            for char in message[start:]:
+                if char in level:
+                    step_ins += 1
+                    if self.delimit not in level[ char ]:
+                        level = level[ char ]
+                    else:
+                        ret.append( repl*step_ins )
+                        start += step_ins - 1
+                        break
+                else:
+                    ret.append( message[start] )
+                    break
+            start += 1
+
+        return ''.join(ret)
+
+
+if __name__ == "__main__":
+    #gfw = NaiveFilter()
+    #gfw = BSFilter()
+    gfw = DFAFilter()
+    gfw.parse("keywords")
+    import time
+    t= time.time()
+    print gfw.filter("法轮功 我操操操","*")
+    print gfw.filter("针孔摄像机 我操操操","*")
+    print gfw.filter("售假人民币 我操操操","*")
+    print gfw.filter("传世私服 我操操操","*")
+    print time.time() - t
